73416ef488961d9da62fd6c8e95052e2a98ab9f9
kennethbasco <kpb82@scarletmail.rutgers.edu>
2018-06-26 17:59:16 -0400

    enemies can now hurt player shield

--- Assets/_Scripts/Hero.cs
+++ Assets/_Scripts/Hero.cs
@@ -19,11 +19,16 @@ public class Hero : MonoBehaviour
 
     public float pitchMult = 30;
 
+    public float gameRestartDelay = 2f;
 
 
     [Header("Set Dynamically")]
 
-    public float shieldLevel = 1;
+    [SerializeField]
+
+    private float _shieldLevel = 1;
+
+    private GameObject lastTriggerGo = null;
 
 
 
@@ -76,4 +81,75 @@ public class Hero : MonoBehaviour
         transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
 
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        Transform rootT = other.gameObject.transform.root;
+
+        GameObject go = rootT.gameObject;
+
+        //print("Triggered: " + go.name);
+
+        if (go == lastTriggerGo)
+        {                                           // c
+
+            return;
+
+        }
+
+        lastTriggerGo = go;                                                  // d
+
+
+
+        if (go.tag == "Enemy")
+        {  // If the shield was triggered by an enemy
+
+            shieldLevel--;        // Decrease the level of the shield by 1
+
+            Destroy(go);          // … and Destroy the enemy                 // e
+
+        }
+        else
+        {
+
+            print("Triggered by non-Enemy: " + go.name);                       // f
+
+        }
+
+    }
+
+
+    public float shieldLevel
+    {
+
+        get
+        {
+
+            return (_shieldLevel);                                          // a
+
+        }
+
+        set
+        {
+
+            _shieldLevel = Mathf.Min(value, 4);                             // b
+
+            // If the shield is going to be set to less than zero
+
+            if (value < 0)
+            {                                                 // c
+
+                Destroy(this.gameObject);
+
+                Main.S.DelayedRestart(gameRestartDelay);
+
+
+            }
+
+        }
+
+    }
+
 }
